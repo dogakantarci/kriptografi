97f89971f4267064bd102fd42333ef933a8c0126
doa <dogakantarci@gmail.com>
2025-03-18 01:18:29 +0300

    feat: diffie-hellman

---
+++ diffie-hellman.py
@@ -0,0 +1,18 @@
+import rsa 
+import random
+from sympy import primitive_root
+
+def diffie_hellman():
+    p = rsa.create_prime()
+    g = primitive_root(p)
+    a = random.randint(100000,1000000)
+    b = random.randint(100000,1000000)
+    print(p, g, a, b)
+    A = pow(g, a, p)
+    B = pow(g, b, p)
+
+    key_alice = pow(A, b, p)
+    key_bob = pow(B, a ,p)
+    print(key_alice, key_bob)
+    return(key_alice, key_bob)
+diffie_hellman()
--- rsa.py
+++ rsa.py
@@ -1,11 +1,11 @@
 import random
 import math
 
-def miller_rabin(n):
-    s, d = 0, n-1
-    result = "NO!"
-    
-    while(d % 2 == 0):
+def miller_rabin(n):    # n-1'i 2**s * d olarak hesapla
+    s, d = 0, n-1       # sonra d kalana kadar 2'ye bölüyoruz
+    result = "NO!"      # 8 farklı rastgele a var, eğer a**d mod n (n-1) veya 1 değilse
+                        # 2'nin kuvvetini 1 artırıyorum ve yeniden s'ye kadar deniyoruz
+    while(d % 2 == 0):  # false dönerse sayı asal değildir
         d //= 2
         s += 1
     #print(n,n-1,s,d)
@@ -41,23 +41,22 @@ def create_prime():
 
 def rsa_key():
     d = 0
-    p = create_prime() # 1. adım
+    p = create_prime() # 1. adım 2 tane asal sayı
     q = create_prime()
     #print(p,q)
-    n = p * q # 2. adım
-    lambda_n = math.lcm(p-1, q-1) # 3. adım
+    n = p * q # 2. adım onların çarpımları
+    lambda_n = math.lcm(p-1, q-1) # 3. adım sayıların 1 eksiğinin ekoku
     while(True):
-        e = random.randint(1, lambda_n)
-        if(math.gcd(e, lambda_n) == 1):
+        e = random.randint(1, lambda_n) # e ile lambda_n aralarında asal olmalı
+        if(math.gcd(e, lambda_n) == 1): # aralarında asallık kontrolü
             break
 
-    for i in range(1, lambda_n):  
-        if (e * i) % lambda_n == 1:
-            d = i
-            break
+    d = pow(e, -1, lambda_n)  # e'nin mod lambda_n'ye göre tersini alıyoruz
 
 
-    print((n, e), (d, p, q, lambda_n))
-    return((n, e), (d, p, q, lambda_n))
 
+    #print((n, e), (d, p, q, lambda_n)) # genel anahtarlar, özel anahtarlar
+    return((n, e), (d, p, q, lambda_n)) 
+ 
 rsa_key()
+
